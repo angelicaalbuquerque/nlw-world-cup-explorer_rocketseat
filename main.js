67205b3641dfc6d0fa3a9e53ac5f379bebc08eb9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-11-21 09:07:30 -0300

    Refactoring

--- main.js
+++ main.js
@@ -19,18 +19,12 @@ function createCard(date, day, games) {
   `;
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-<img src="./assets/logo.svg" alt="Logo NLW Copa">
-</header>
-<main id="cards">
-${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-${createCard(
-  "28/11",
-  "segunda",
-  createGame("switzerland", "13:00", "brazil") +
-    createGame("portugal", "16:00", "uruguai"),
-)}
-${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
-</main>
-`;
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("switzerland", "13:00", "brazil") +
+      createGame("portugal", "16:00", "uruguai"),
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"));
